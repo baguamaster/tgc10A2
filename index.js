5453d1f00849756e85ec218d4ce9c1b04212e666
Kevin Ho <traderkev54@yahoo.com>
2021-01-23 04:01:40 +0800

    search callback submission

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ $("#form").submit(function(e) {
 
             var API_KEY = '91e67499c338b646b3590f73a5696583'
 
-            var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + '&type=web &query=${query}'
+            var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + '&query=${query}'
             console.log(url)
 
 
@@ -16,13 +16,12 @@ $("#form").submit(function(e) {
                         console.log(data)
 
                         data.organic_results.forEach(res => {
-
                             result = ` <
-          h1 > $ { res.title } < /h1><br><a target="_blank href="$(res.url}">$<res.url}</a >
-          <
-          p > $ { res.snippet } < /p>
-  
-      `
+            h1 > $ { res.title } < /h1><br><a target="_blank href="$(res.url}">$<res.url}</a >
+            <
+            p > $ { res.snippet } < /p>
+    
+        `
 
 
                             $("#result").append(result)
