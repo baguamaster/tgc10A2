2be5c7415bb50579bb3938a6bb17b3e851aee936
Kevin Ho <traderkev54@yahoo.com>
2021-01-23 04:10:26 +0800

    further adjustments to js

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-$("#form").submit(function(e) {
-            e.preventDefault()
+$("#form").submit(function (e) {
+  e.preventDefault()
 
-            var query = $("#search").val()
+  var query = $("#search").val()
 
-            var API_KEY = '91e67499c338b646b3590f73a5696583'
+  var API_KEY = '91e67499c338b646b3590f73a5696583'
 
-            var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + '&query=${query}'
-            console.log(url)
-
-
-            $.get(url, function(data) {
-
-                        $("#result").html('')
-
-                        console.log(data)
-
-                        data.organic_results.forEach(res => {
-                            result = ` <
-            h1 > $ { res.title } < /h1><br><a target="_blank href="$(res.url}">$<res.url}</a >
-            <
-            p > $ { res.snippet } < /p>
-    
-        `
+  var url = 'http://api.serpstack.com/search?access_key=' + API_KEY + '&query=${query}'
+  console.log(url)
 
 
-                            $("#result").append(result)
+  $.get(url, function (data) {
 
-                        })
+    $("#result").html('')
+
+    console.log(data)
+
+    data.organic_results.forEach(res => {
+      result = `
+      
+      <h1>${res.title}</h1><br><a target="_blank href="${res.url}">${res.url}</a>
+      <p>${res.snippet}</p>`
+      
+      $("#result").append(result)
+
+    });
